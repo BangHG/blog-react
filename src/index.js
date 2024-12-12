8d733a9b0d7da02e9a1ebf6bbd505a46a8aafbcd
BangHG <42599226+BangHG@users.noreply.github.com>
2024-12-12 16:05:55 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 ﻿// #21.5.4 라우트 모듈화
+require('dotenv').config();// #22 mongoose를 이용한 mongoDB연동 실습
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser'); //#21.5.5.2 컨트롤러 파일작성
+const mongoose = require('mongoose')//22.3.2 mongoose로 서버와 데이터베이스연결
 
 const api = require('./api');
+
+//비구조화 할당을 통해 precoess.env 내부 값에 대한 레퍼런스 만들기
+const {PORT,MONGO_URI} = process.env;
+
+mongoose.connect(MONGO_URI).then(()=>{
+  console.log('몽고DB연결');
+}).catch(e=>{
+  console.error(e)
+})
+/* 
+<!-- 230209 수정 -->
+하 몽고디비 설치에서 막혔다.
+아무리해도안된다,.,..,,..,.
+ */
+
 
 const app = new Koa();
 const router = new Router();
@@ -15,6 +32,7 @@ app.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('4000 listening');
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('listening to port %d',port);  
 });
